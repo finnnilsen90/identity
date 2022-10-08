bbfd27ddb18ada395c3c5f0e628016c9c3b80778
finnnilsen90 <fnilsen1051@gmail.com>
2022-10-08 16:06:35 -0700

    Created overlap_thought_experiment

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Identity
 With the browser cookie being sunset in the next 2 years (if Google ever really decides to do it) it is going to be vital to build in new mechanisms for identifying and segmenting audiences anonymously. This repo will be used to store data analysis exercises around this concept.
 
+## Identity Philosophy
+The approach this repo takes is to mimic real-world conditions when identifying PII data on the internet and how to securely store and reidentify PII data later. Below bullet points outline the overall approach and how the approach is intended to achieve the goals of this repo.
+- Create a custom module that stores ways to create, anonymize, and analyze FAKE PII datasets. This module is meant to consolidate code on jupyter notebooks and offer a plug and play tool to use in various exercizes and usecases.
+- Develop thought experiments designed to analyze and mimic how anonymized data is identified securly and looked up against overlapped datasets.
+- Through these thought experiments mimic real world conditions that impact match rate and scale.
+
 # Custom Modules
-The below section lists out all of the custom modules used for the various excercizes and analysis.
+The below section lists out all of the custom modules used for the various exercises and analysis.
 
 ## everyone_anonymous 
 The **everyone_anonymous** module is used to store tools and methods for analyzing and modeling anonymous datasets.
@@ -34,11 +40,18 @@ data = {
 When thinking of ways to anonymously identify users on the internet one of the most important questions to answer is how is scale going to be impacted? The goal of thess thought experiments is to model different hypothetical scenarios and build tools to better answer business questions on this topic.
 
 ## [general_thought_experiment](https://github.com/finnnilsen90/identity/blob/main/general_thought_experiment.ipynb)
-A 100,000 user dataset with PII from a company called Finn Corp is hashed utilizing the email address. These hashed emails are used by the DSP to identify users that login to a publishers website. Determine the scale when targeting those hashed email addresses in a $5,000 display campaign.
+A 100,000-user dataset with PII from a company called Finn Corp is hashed utilizing the email address. These hashed emails are used by the DSP to identify users that login to a publisher’s website. Determine the scale when targeting those hashed email addresses in a $5,000 display campaign.
 
 ### Supply Side Business Questions
 - What percent of users login to a publisher website?
 - Of those logged in users what percent of them are in our dataset?
 ### Buy Side Business Questions
-- What is the frequency per hashed email over the lifetime of a 30 day digital campaign?
-- How much is spent to reach the matched emails with a 4 dollar CPM?
+- What is the frequency per hashed email over the lifetime of a 30-day digital campaign?
+- How much is spent to reach the matched emails with a 4-dollar CPM?
+
+## [overlap_thought_expriment]()
+A 100,000-user dataset with PII from a company called Finn Corp is compared to a 500,000 dataset with PII that includes demographic data. The goal of this overlap analysis is to first see what the % overlap is and second define demographic information.
+
+### Business Questions
+- What is the percent overlap between the 2 datasets?
+- What is the demographic information of the matched dataset?
--- everyone_anonymous.py
+++ everyone_anonymous.py
@@ -9,22 +9,75 @@ names = [i for i in names['Name']]
 # Make something from nothing to test FAKE anonomyzed data. 
 class something_nothing:
 
-    def __init__(self,data,nrows):
+    def __init__(self,data=None,nrows=0):
         self.data = data
         self.nrows = nrows+1
     # Generate a random dataset to represent data with PII.
-    def generate_data(self):
+    def generate_data(self,gender_scew=50,age_min=18,age_max=80,hhi_scew=800000):
         data = self.data
+        data_len = self.nrows
 
-        for i in range(self.nrows):
+        for i in range(data_len):
+            
+            # Basic PII that should always be included in the dataset.
             rand_first = random.randrange(len(names))
             rand_last = random.randrange(len(names))
+            try:
+                data['First_Name'].append(names[rand_first])
+                data['Last_Name'].append(names[rand_last])
+                data['Email'].append(data['First_Name'][i]+data['Last_Name'][i]+'@gmail.com')
+            except KeyError as e:
+                if data['First_Name'] == e:
+                    data['Last_Name'].clear() 
+                else:
+                    data['First_Name'].clear()
+                data = 'Missing a '+str(e)+'. Please add a '+str(e)+' to the dictionary.'
+                break
 
-            data['First_Name'].append(names[rand_first])
-            data['Last_Name'].append(names[rand_last])
-            data['Email'].append(data['First_Name'][i]+data['Last_Name'][i]+'@gmail.com')
+            # Optional PII that may or maynot be included in the dataset.
+            create_demo_data = self.demo_data(gender_scew,age_min,age_max,hhi_scew)
 
+            try:
+                data['Gender'].append(create_demo_data['Gender'])
+                data['Age'].append(create_demo_data['Age'])
+                data['HHI'].append(create_demo_data['HHI'])
+            except KeyError:
+                continue
+     
         return data
+    
+    def demo_data(self,gender_scew,age_min,age_max,hhi_scew):
+        
+        rand_gender = random.randrange(100)
+        if rand_gender > gender_scew:
+            gender = 'Male'
+        else:
+            gender = 'Female'
+
+        rand_age = random.randrange(age_min,age_max)
+
+        rand_hhi = random.randrange(hhi_scew)
+
+        hhi_value = 100000 + hhi_scew
+
+        if rand_hhi < 50000:
+            hhi = 'Less than $50,000'
+        elif rand_hhi >= 50001 and rand_hhi < 100000:
+            hhi = '\$50,001 - $100,000'
+        elif rand_hhi >= 100001 and rand_hhi < 200000:
+            hhi = '\$100,001 - $200,000'
+        elif rand_hhi >= 200001 and rand_hhi < 500000:
+            hhi = '\$200,001 - $500,000'
+        elif rand_hhi > 500000:
+            hhi = 'More than $500,000'
+
+        demo_obj = {
+            'Gender': gender,
+            'Age': rand_age,
+            'HHI': hhi
+        }
+
+        return demo_obj
 
     # Hash emails.
     def hash(self):
@@ -46,3 +99,33 @@ class something_nothing:
             email = email_arr[i]
             if hash(email) == hashed:
                 return email
+
+# Add something to nothing to build FAKE PII datasets.
+
+class add_something:
+
+    def __init__(self,data=None,nrows=0):
+        self.data = data
+        self.nrows = nrows
+    
+    def add_demo_data(self,gender=True,age=True,hhi=True,gender_scew=50,age_min=18,age_max=80,hhi_scew=800000):
+        
+        data = self.data
+
+        if gender:
+            data.update({'Gender': []})
+        if age:
+            data.update({'Age': []})
+        if hhi:
+            data.update({'HHI': []})
+
+        for i in range(self.nrows):
+            demo_data = something_nothing().demo_data(gender_scew,age_min,age_max,hhi_scew)
+            try:
+                data['Gender'].append(demo_data['Gender'])
+                data['Age'].append(demo_data['Age'])
+                data['HHI'].append(demo_data['HHI'])
+            except KeyError:
+                continue
+
+        return data
